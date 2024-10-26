757cf175b9643cf12c25477084597300c6b7c18e
himbalt0okm <elnikov.mihael@yandex.com>
2024-10-26 11:25:24 +0300

    Task 6.2b выполнен

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,37 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+address = input()
+correct = False
+
+while correct == False:    
+    try:
+        address_splited = address.split('.')
+        if len(address_splited) != 4:
+            correct = False
+        else:
+            for octet in address_splited:
+                if not(0 <= int(octet) <= 255):
+                    correct = False
+                else:
+                    correct = True
+    except ValueError:
+        correct = False
+    finally:
+        if correct == False:
+            print('Неправильный IP-адрес')
+            address = input()
+            continue
+    
+if correct == True:
+    if 1 <= int(address_splited[0]) <= 223:
+        print('unicast')
+    elif 224 <= int(address_splited[0]) <= 239:
+        print('multicast')
+    elif address == '255.255.255.255':
+        print('local broadcast')
+    elif address == '0.0.0.0':
+        print('unassigned')
+    else:
+        print('unused')
