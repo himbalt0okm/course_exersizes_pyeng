2dfbc2bdf3cb7bf085e0b056f906e2a78c153aba
himbalt0okm <elnikov.mihael@yandex.com>
2024-10-28 11:53:54 +0300

    Task 7.1 выполнен

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,3 +14,22 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+with open('ospf.txt', 'r') as f: 
+    for line in f:
+        r_dict = {'Prefix': '',               
+                'AD/Metric':'' ,            
+                'Next-Hop':'',              
+                'Last update':'',           
+                'Outbound Interface':''}
+        
+        line_list = line.split()
+        r_dict['Prefix'] = line_list[1]
+        r_dict['AD/Metric'] = line_list[2].replace('[', '').replace(']', '')
+        r_dict['Next-Hop'] = line_list[4]
+        r_dict['Last update'] = line_list[5]
+        r_dict['Outbound Interface'] = line_list[6]
+        #print(r_dict)
+        for key in r_dict:
+            print('{:<22}{:<15}'.format(key, r_dict[key]))
+        print('\n')
