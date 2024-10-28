06b729520c2f7c77c1d065872b372addc071ef69
himbalt0okm <elnikov.mihael@yandex.com>
2024-10-28 14:45:03 +0300

    Task 7.3b выполнен

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -17,3 +17,25 @@ Enter VLAN number: 10
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+user_number = input('Enter VLAN number: ')
+
+with open("CAM_table.txt", 'r') as f:
+    lines = f.readlines()
+    result_lst = []
+    i=1
+    while i <=6:
+        lines.pop(0)
+        i += 1
+        
+    for line in lines:
+        line = line.strip()
+        line = line.split()
+        if line[0] == user_number:
+        #result_str = 
+            result_str = [int(line[0]), line[1],line[3]]
+            result_lst.append(result_str)
+            result_lst.sort()
+    for line in result_lst:
+        line = '{:<9}{:<20}{:<0}'.format(str(line[0]), line[1],line[2])
+        print(line)
